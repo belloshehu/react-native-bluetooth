b4b1416c4a1384d0f27289aa75bb66cc4b839aaa
Belloshehu <belloshehu1@gmail.com>
2024-06-29 23:30:22 +0100

    Add bluetooth device list

--- App.js
+++ App.js
@@ -1,20 +1,225 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  Platform,
+  StatusBar,
+  ScrollView,
+  StyleSheet,
+  Dimensions,
+  SafeAreaView,
+  NativeModules,
+  useColorScheme,
+  TouchableOpacity,
+  NativeEventEmitter,
+  PermissionsAndroid,
+} from "react-native";
+import BleManager from "react-native-ble-manager";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { DevicesList } from "./components/DevicesList";
 
-export default function App() {
+const BleManagerModule = NativeModules.BleManager;
+const BleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+
+const App = () => {
+  const [isScanning, setIsScanning] = useState(false);
+  const [connectedPeripherals, setConnectedPeripherals] = useState([]);
+  const [discoveredDevices, setDiscoveredDevices] = useState([]);
+  const peripherals = new Map();
+
+  useEffect(() => {
+    BleManager.enableBluetooth().then(() => {
+      console.log("Bluetooth is turned on");
+    });
+
+    BleManager.start({ showAlert: false }).then(() => {
+      console.log("Bluetooth manager started");
+    });
+
+    // get discovered devices
+    const stopDiscoverListener = BleManagerEmitter.addListener(
+      "BleManagerDiscoverPeripheral",
+      (peripheral) => {
+        peripherals.set(peripheral.id, peripheral);
+        setDiscoveredDevices(Array.from(peripherals.values()));
+      }
+    );
+
+    const stopConnectListener = BleManagerEmitter.addListener(
+      "BleManagerConnectPeripheral",
+      (peripheral) => {
+        console.log("BleManagerConnectPeripheral", peripheral);
+      }
+    );
+
+    const stopScanListener = BleManagerEmitter.addListener(
+      "BleManagerStopScan",
+      () => {
+        setIsScanning(false);
+        console.log("stopped scanning");
+      }
+    );
+
+    // permission check
+
+    if (Platform.OS === "android" && Platform.Version >= 23) {
+      PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      ).then((result) => {
+        if (result) {
+          console.log("permission is ok");
+        } else {
+          PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+          ).then((result) => {
+            if (result) {
+              console.log("User accepted permission");
+            } else {
+              console.log("User rejected permission");
+            }
+          });
+        }
+      });
+    }
+
+    return () => {
+      stopDiscoverListener.remove();
+      stopConnectListener.remove();
+      stopScanListener.remove();
+    };
+  }, []);
+
+  const handleGetConnectedPeripherals = () => {
+    BleManager.getConnectedPeripherals([]).then((results) => {
+      if (results.length === 0) {
+        console.log("No connecrted peripheral found");
+      } else {
+        for (let i = 0; i < results.length; i++) {
+          let peripheral = results[i];
+          peripheral.connected = true;
+          peripherals.set(peripheral.id, peripheral);
+          setConnectedPeripherals(Array.from(peripherals.values()));
+        }
+      }
+    });
+  };
+
+  // pair with device first before connecting to it
+  const connectToPeripheral = (peripheral) => {
+    BleManager.createBond(peripheral.id)
+      .then(() => {
+        peripheral.connected = true;
+        peripherals.set(peripheral.id, peripheral);
+        setConnectedPeripherals(Array.from(peripherals.values()));
+        setDiscoveredDevices(Array.from(peripherals.values()));
+        console.log(`Successfully paired with ${peripheral.name}`);
+      })
+      .catch((error) => {
+        console.log(`Failed to connect to ${peripheral.name}`);
+      });
+  };
+
+  // disconnect from a paired device
+  const diconnectedPairedPeripheral = (peripheral) => {
+    BleManager.disconnect(peripheral.id).then(() => {
+      peripheral.connected = false;
+      peripherals.set(peripheral.id, peripheral);
+      setDiscoveredDevices(Array.from(peripherals.values()));
+      setConnectedPeripherals(Array.from(peripherals.values()));
+    });
+  };
+
+  const startScan = () => {
+    if (!isScanning) {
+      BleManager.scan([], 5, true)
+        .then(() => {
+          setIsScanning(true);
+          console.log("scanning");
+        })
+        .catch((error) => {
+          console.log("Scanning error", error);
+        });
+    }
+  };
+  const isDarkMode = useColorScheme() === "dark";
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[backgroundStyle, styles.mainBody]}>
+      <StatusBar
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        style={backgroundStyle}
+        contentContainerStyle={styles.mainBody}
+        contentInsetAdjustmentBehavior="automatic">
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+            marginBottom: 40,
+          }}>
+          <View>
+            <Text
+              style={{
+                fontSize: 30,
+                textAlign: "center",
+                color: isDarkMode ? Colors.white : Colors.black,
+              }}>
+              React Native BLE Manager Tutorial
+            </Text>
+          </View>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            style={styles.buttonStyle}
+            onPress={startScan}>
+            <Text style={styles.buttonTextStyle}>
+              {isScanning ? "Scanning" : "Scan Bluetooth Devices"}{" "}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+      <DevicesList
+        devices={discoveredDevices}
+        title={"Discovered devices"}
+        connectHandler={connectToPeripheral}
+        disconnectHandler={diconnectedPairedPeripheral}
+      />
+      <DevicesList
+        devices={connectedPeripherals}
+        title={"Connected devices"}
+        connectHandler={connectToPeripheral}
+        disconnectHandler={diconnectedPairedPeripheral}
+      />
+    </SafeAreaView>
   );
-}
+};
+const windowHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
-  container: {
+  mainBody: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    height: windowHeight,
+  },
+  buttonStyle: {
+    backgroundColor: "#307ecc",
+    borderWidth: 0,
+    color: "#FFFFFF",
+    borderColor: "#307ecc",
+    height: 40,
+    alignItems: "center",
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 15,
+  },
+  buttonTextStyle: {
+    color: "#FFFFFF",
+    paddingVertical: 10,
+    fontSize: 16,
   },
 });
+export default App;
---
+++ components/Device.js
@@ -0,0 +1,37 @@
+import { View, Touchable, Text } from "react-native";
+import { styles } from "../styles";
+
+/* render list of bluetooth devices */
+export const Device = ({ peripheral, connect, disconnect }) => {
+  const { name, rssi, connected } = peripheral;
+  return (
+    <>
+      {name && (
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginBottom: 10,
+          }}>
+          <View style={styles.deviceItem}>
+            <Text style={styles.deviceName}>{name}</Text>
+            <Text style={styles.deviceInfo}>RSSI: {rssi}</Text>
+          </View>
+          <TouchableOpacity
+            onPress={() =>
+              connected ? disconnect(peripheral) : connect(peripheral)
+            }
+            style={styles.deviceButton}>
+            <Text
+              style={[
+                styles.scanButtonText,
+                { fontWeight: "bold", fontSize: 16 },
+              ]}>
+              {connected ? "Disconnect" : "Connect"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </>
+  );
+};
